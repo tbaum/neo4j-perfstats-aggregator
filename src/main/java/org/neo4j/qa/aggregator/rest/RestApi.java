e376b9a5c4ab4729c58eafd399d1ab74e327b08f
Thomas Baum <thomas.baum@atns.de>
2011-03-15 12:00:43 +0100

    add visualization

--- src/main/java/org/neo4j/qa/aggregator/rest/RestApi.java
+++ src/main/java/org/neo4j/qa/aggregator/rest/RestApi.java
@@ -12,15 +12,11 @@ import org.springframework.orm.jpa.JpaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceException;
-import javax.persistence.TypedQuery;
+import javax.persistence.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Path("/")
 @Component
@@ -61,6 +57,94 @@ public class RestApi {
                     }
                 }
             });
+        } catch (Exception e) {
+            return Response
+                    .status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(new JSONStringer().object().key("message").value(e.getMessage()).endObject().toString())
+                    .build();
+        }
+    }
+
+
+    @GET
+    @Path("/graph/{component}/{version}")
+    @Produces("text/plain")
+    @Transactional
+    public Response getChart(@PathParam("component") final String component,
+                             @PathParam("version") final String version) throws JSONException {
+
+        return (Response) new JpaTemplate(emf).execute(new JpaCallback<Object>() {
+            public Object doInJpa(EntityManager entityManager) throws PersistenceException {
+                TypedQuery<StatisticRecord> q = entityManager.createQuery("FROM StatisticRecord " +
+                        " WHERE component=:component AND version=:version order by timestamp desc", StatisticRecord.class);
+                q.setParameter("component", component);
+                q.setParameter("version", version);
+                q.setMaxResults(20);
+
+                StringBuilder s = new StringBuilder();
+                List<String> keys = new ArrayList<String>();
+                for (StatisticRecord statisticRecord : q.getResultList()) {
+                    for (String s1 : statisticRecord.getValues().keySet()) {
+                        if (!keys.contains(s1)) {
+                            keys.add(s1);
+                        }
+                    }
+                }
+
+                s.append("Timestamp");
+                for (String key : keys) {
+                    s.append(",").append(key);
+                }
+                s.append("\n");
+
+                for (StatisticRecord statisticRecord : q.getResultList()) {
+                    s.append(new SimpleDateFormat("MM/dd/yy HH:mm").format(  statisticRecord.getTimestamp()));
+
+                    for (String key : keys) {
+                        Double v = statisticRecord.getValues().get(key);
+                        s.append(",").append(v != null ? v : "");
+                    }
+                    s.append("\n");
+
+                }
+                return Response.status(Response.Status.OK).entity(s.toString()).build();
+
+
+            }
+        });
+
+
+    }
+
+    @GET
+    @Path("/graph")
+    @Produces("application/json")
+    @Transactional
+    public Response getChart() throws JSONException {
+        try {
+            return (Response) new JpaTemplate(emf).execute(new JpaCallback<Object>() {
+                public Object doInJpa(EntityManager entityManager) throws PersistenceException {
+                    try {
+
+                        Query q = entityManager.createQuery("SELECT component,version FROM StatisticRecord " +
+                                " group by component,version");
+
+                        JSONWriter res = new JSONStringer().array();
+
+                        List<Object[]> resultList = q.getResultList();
+                        for (Object[] o : resultList) {
+                            res.value(o[0] + "/" + o[1]);
+                        }
+                        res.endArray();
+
+                        return Response.status(Response.Status.OK).entity(res.toString()).build();
+
+                    } catch (JSONException e) {
+                        throw new PersistenceException(e);
+                    }
+                }
+            });
+
         } catch (Exception e) {
             return Response
                     .status(Response.Status.INTERNAL_SERVER_ERROR)
